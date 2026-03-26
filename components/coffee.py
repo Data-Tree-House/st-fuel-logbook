439a1280b1f97df59252098382c2f94fdc98424f
Johandielangman <jghanekom2@gmail.com>
2026-03-26 07:48:16 +0200

    fix: cpa the size of the coffee logo

--- components/coffee.py
+++ components/coffee.py
@@ -4,4 +4,7 @@ import streamlit as st
 def buy_us_a_coffee():
     with st.sidebar:
         st.divider()
-        st.markdown("[![buy-us-a-coffee](./app/static/buy-us-a-coffee.png)](https://pos.snapscan.io/qr/Ew6rBAsV)")
+        st.markdown(
+            "[![buy-us-a-coffee](./app/static/buy-us-a-coffee.png)](https://pos.snapscan.io/qr/Ew6rBAsV)",
+            width=350,
+        )
